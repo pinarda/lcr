4066f19fe5edb10fe8fcb612461b690ad44b0ff1
Alex <pinard.alex@gmail.com>
2022-04-18 13:47:07 -0600

    test

--- lcr/cheyenne_batch_alex.sh
+++ lcr/cheyenne_batch_alex.sh
@@ -1,10 +1,10 @@
 #!/bin/tcsh
-#PBS -A NTDD0004
+#PBS -A NTDD0005
 #PBS -N testb
 #PBS -q regular
 #PBS -l walltime=0:20:00
 #PBS -j oe
-#PBS -M abaker@ucar.edu
+#PBS -M apinard@ucar.edu
 #PBS -l select=1:ncpus=1:mem=109GB
 
 module load conda
@@ -13,7 +13,7 @@ conda activate ldcpy_env
 setenv TMPDIR /glade/scratch/$USER/temp
 mkdir -p $TMPDIR
 
-python ./compute_batch.py -o '/glade/scratch/apinard/chey_calcs_daily.csv' -j 'alex.json' -tt 10 -ts 10
+python ./compute_batch.py -o '/glade/scratch/apinard/chey_calcs_daily.csv' -j 'alex.json' -tt 10 -ts 10 -v -ld "/glade/u/home/apinard/newldcpy/ldcpy"
 
 
 
