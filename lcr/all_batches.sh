93235ed4efb023dd5b90f4d048e4de748c83b518
Alex <pinard.alex@gmail.com>
2022-05-16 01:22:05 -0600

    update

--- lcr/all_batches.sh
+++ lcr/all_batches.sh
@@ -2,5 +2,5 @@
 
 set arrname = (bc_a1_SRF dst_a1_SRF dst_a3_SRF FLNS FLNSC FLUT FSNS FSNSC FSNTOA ICEFRAC LHFLX pom_a1_SRF PRECL PRECSC PRECSL PRECT PRECTMX PSL Q200 Q500 Q850 QBOT SHFLX so4_a1_SRF so4_a2_SRF so4_a3_SRF soa_a1_SRF soa_a2_SRF T010 T200 T500 T850 TAUX TAUY TMQ TREFHT TREFHTMN TREFHTMX TS U010 U200 U500 U850 VBOT WSPDSRFAV Z050 Z500)
 foreach x ($arrname)
-qsub batch_scripts/bg_dssim_scripts/cheyenne_batch_$x.sh
+qsub batch_scripts/cheyenne_batch_$x.sh
 end
