5022af5e5c62e854f2457229c79aa3fd9ac1f1b7
Alex <pinard.alex@gmail.com>
2022-05-26 16:05:52 -0600

    fix

--- lcr/batch_scripts/sz_dssim_scripts/cheyenne_batch_bc_a1_SRF.sh
+++ lcr/batch_scripts/sz_dssim_scripts/cheyenne_batch_bc_a1_SRF.sh
@@ -13,7 +13,7 @@ conda activate ldcpy_env
 setenv TMPDIR /glade/scratch/$USER/temp
 mkdir -p $TMPDIR
 
-python ./compute_batch.py -o '/glade/scratch/apinard/sz3/bc_a1_SRF_calcs.csv' -j './batch_scripts/sz_dssim_scripts/bc_a1_SRF.json' -v -ld
+python ./compute_batch.py -o '/glade/scratch/apinard/sz3/bc_a1_SRF_calcs.csv' -j './batch_scripts/sz_dssim_scripts/bc_a1_SRF.json' -v -ld -tt 1
 
 
 
