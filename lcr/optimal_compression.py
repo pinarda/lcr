008ad78350e39c1c5083eab8aa79d42f64ab3c91
Alex <pinard.alex@gmail.com>
2022-06-01 12:17:06 -0600

    fix

--- lcr/optimal_compression.py
+++ lcr/optimal_compression.py
@@ -343,7 +343,7 @@ def main_zfp(argv):
     print(f"current_var: {argv_var}")
 
     for freq in ['daily']:
-        v = lcr_global_vars.varlist(f"../data/{freq}_dssims.csv")
+        #v = lcr_global_vars.varlist(f"../data/{freq}_dssims.csv")
         #for argv_var in v:
         location = f"../data/2real_zfp_bg_sz_comp_slices.csv"
         #location = f"../data/monthly_zfp_bg_sz_comp_slices.csv"
