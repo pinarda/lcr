64e6e6ffe6ef41ba2f11b37816a7ca5d628624af
Alex <pinard.alex@gmail.com>
2022-07-06 10:47:50 -0600

    fix

---
+++ lcr/monthly_cheyenne_scripts/orig/CO2_OCN.sh
@@ -0,0 +1,19 @@
+#!/bin/tcsh
+#PBS -A NTDD0005
+#PBS -N testb
+#PBS -q regular
+#PBS -l walltime=12:00:00
+#PBS -j oe
+#PBS -M apinard@ucar.edu
+#PBS -l select=1:ncpus=1
+
+module load conda
+#conda activate my-npl-ml
+conda activate ldcpy_env
+
+setenv TMPDIR /glade/scratch/$USER/temp
+mkdir -p $TMPDIR
+
+setenv HDF5_PLUGIN_PATH /glade/work/haiyingx/H5Z-ZFP-PLUGIN-unbiased/plugin
+
+python ../data_gathering/compute_batch.py -o '/glade/scratch/apinard/orig_CO2_OCN.csv' -j 'orig/CO2_OCN.json' -tt 100 -ld
