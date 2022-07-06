4a702d9f6274076535e0a264e1636cfb4834ca89
Alex <pinard.alex@gmail.com>
2022-07-05 18:51:01 -0600

    fix

--- lcr/monthly_cheyenne_scripts/clean.sh
+++ lcr/monthly_cheyenne_scripts/clean.sh
@@ -3,7 +3,7 @@
 rm -f testb*
 rm /glade/scratch/apinard/*.csv
 
-output=$(qstat -u apinard | tail -n +4 | cut -d '.' -f 1)
+set output=$(qstat -u apinard | tail -n +4 | cut -d '.' -f 1)
 foreach x ($output)
   qdel $x.chadmin1.ib0.cheyenne.ucar.edu
 end
