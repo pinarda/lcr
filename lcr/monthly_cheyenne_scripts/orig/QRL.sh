cc50b7412d2fde2d565c6557a8d6e6540c063f25
Alex <pinard.alex@gmail.com>
2022-07-06 15:47:03 -0600

    orig fix

--- lcr/monthly_cheyenne_scripts/orig/QRL.sh
+++ lcr/monthly_cheyenne_scripts/orig/QRL.sh
@@ -16,4 +16,4 @@ mkdir -p $TMPDIR
 
 setenv HDF5_PLUGIN_PATH /glade/work/haiyingx/H5Z-ZFP-PLUGIN-unbiased/plugin
 
-python ../data_gathering/compute_batch.py -o '/glade/scratch/apinard/orig_QRL.csv' -j 'orig/QRL.json' -tt 100 -ld
+python ../data_gathering/compute_batch.py -oo '/glade/scratch/apinard/orig_QRL.csv' -j 'orig/QRL.json' -tt 100 -ld
--- lcr/monthly_cheyenne_scripts/orig/TOT_CLD_VISTAU.sh
+++ lcr/monthly_cheyenne_scripts/orig/TOT_CLD_VISTAU.sh
@@ -16,4 +16,4 @@ mkdir -p $TMPDIR
 
 setenv HDF5_PLUGIN_PATH /glade/work/haiyingx/H5Z-ZFP-PLUGIN-unbiased/plugin
 
-python ../data_gathering/compute_batch.py -o '/glade/scratch/apinard/orig_TOT_CLD_VISTAU.csv' -j 'orig/TOT_CLD_VISTAU.json' -tt 100 -ld
+python ../data_gathering/compute_batch.py -oo '/glade/scratch/apinard/orig_TOT_CLD_VISTAU.csv' -j 'orig/TOT_CLD_VISTAU.json' -tt 100 -ld
