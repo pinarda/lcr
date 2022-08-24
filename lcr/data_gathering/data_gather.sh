53117bfef8bd9e8781ba863f3c0a48f24dd3c8e8
Alex <pinard.alex@gmail.com>
2022-08-24 16:57:18 -0600

    fix

--- lcr/data_gathering/data_gather.sh
+++ lcr/data_gathering/data_gather.sh
@@ -23,14 +23,15 @@ set arrDay= (TS)
 #remote mutiple files
 foreach x ($arrDay)
    echo $x
-   set id= printf "tcsh -c 'conda activate my-npl-ml && set prefix=${prefix} && python ~/lcr/lcr/data_gathering/compute_batch.py -oo ~/lcr/data/\${prefix}_calcs/\${prefix}_daily_calcs_${x}.csv -j \${prefix}_calcs.json -ld -tt 10 -v'" | qsub -A NTDD0005 -N testb -q regular -l walltime=12:00:00 -j oe -M apinard@ucar.edu -l select=1:ncpus=1
+   set id = printf "tcsh -c 'conda activate my-npl-ml && set prefix = ${prefix} && python ~/lcr/lcr/data_gathering/compute_batch.py -oo ~/lcr/data/\${prefix}_calcs/\${prefix}_daily_calcs_${x}.csv -j \${prefix}_calcs.json -ld -tt 10 -v'" | qsub -A NTDD0005 -N testb -q regular -l walltime=12:00:00 -j oe -M apinard@ucar.edu -l select=1:ncpus=1
 #  set id2= printf "tcsh -c 'conda activate my-npl-ml && set prefix=${x} && python ~/lcr/lcr/data_gathering/compute_batch.py -o ~/lcr/data/\${prefix}_calcs/\${prefix}_daily_metrics_${x}.csv -j \${prefix}_diff.json -ld -tt 10 -v'" | qsub -A NTDD0005 -N testb -q regular -l walltime=12:00:00 -j oe -M apinard@ucar.edu -l select=1:ncpus=1
 end
 
-while (qstat id)
+while (qstat id > 0)
   echo "waiting"
   sleep 5
 end
+
 echo "done"
 
 # local single file
