24e8930832c5e382eaf2e987172e4157dc176e3a
Alex <pinard.alex@gmail.com>
2022-08-24 21:15:29 -0600

    another try

--- lcr/data_gathering/data_gather.sh
+++ lcr/data_gathering/data_gather.sh
@@ -29,18 +29,16 @@ end
 
 echo $id
 set split = ($id:as/./ /)
-echo $split
-echo $split[1]
-echo qstat $split[1]
-echo `qstat $split[1]`
+
 sleep 30
+echo `qstat $split[1]`
 
 while (1)
   sleep 10
   echo "waiting"
   set out = `qstat $split[1]`
   echo $out
-  if ($out == 0) then
+  if (! $out) then
     break
   endif
 end
