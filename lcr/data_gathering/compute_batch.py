247dfc8d91bfa2c41dc549a2deff2f6ceaca3201
Alex <pinard.alex@gmail.com>
2022-08-29 22:37:24 -0600

    undo

--- lcr/data_gathering/compute_batch.py
+++ lcr/data_gathering/compute_batch.py
@@ -302,17 +302,17 @@ def simple_orig_calcs(
     )
 
     # HERE, compute 2D FFT first
-    #my_data_fft2 = diff_metrics.get_calc("fft2")
-    #fft2_calcs = ldcpy.Datasetcalcs(my_data_fft2, "cam-fv", agg_dims, weighted=False)
+    my_data_fft2 = diff_metrics.get_calc("fft2")
+    fft2_calcs = ldcpy.Datasetcalcs(my_data_fft2, "cam-fv", agg_dims, weighted=False)
 
 
     calc_dict = {}
     for calc in calcs:
         # print(calc)
-        temp = diff_metrics.get_diff_calc(calc).compute()
-        #temp = float(fft2_calcs.get_calc(calc).compute())
-        calc_dict[calc] = temp.item(0)
-        #calc_dict[calc] = temp
+        # temp = diff_metrics.get_diff_calc(calc).compute()
+        temp = float(fft2_calcs.get_calc(calc).compute())
+        # calc_dict[calc] = temp.item(0)
+        calc_dict[calc] = temp
 
     return calc_dict
 
