54904533cf0c563508b1a9f776ec5b6fffa0aaf7
Alex <pinard.alex@gmail.com>
2022-08-31 15:32:44 -0600

    again

--- lcr/data_gathering/compute_batch.py
+++ lcr/data_gathering/compute_batch.py
@@ -243,11 +243,10 @@ def simple_diff_calcs(
 
     agg_dims = ['lat', 'lon']
 
-    ds = ds.to_array()
     
     diff_metrics = ldcpy.Diffcalcs(
-        ds[varname].sel(collection=set1).isel(time=t),
-        ds[varname].sel(collection=set2).isel(time=t),
+        ds[varname].to_array().sel(collection=set1).isel(time=t),
+        ds[varname].to_array().sel(collection=set2).isel(time=t),
         data_type,
         agg_dims,
         spre_tol=0.001
