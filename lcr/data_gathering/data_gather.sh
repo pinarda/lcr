5bf4b562e12934c1a0dfc4fb958538a83686dfec
Alex <pinard.alex@gmail.com>
2022-09-07 09:52:49 -0700

    new set

--- lcr/data_gathering/data_gather.sh
+++ lcr/data_gathering/data_gather.sh
@@ -14,16 +14,16 @@
 # Instructions:
 # To change the daily or monthly variables to be processed, change the variable list arrDay and arrMonth below on lines 24 and 25
 # To change the time slices to be processed, change the starting time slices in line 27 and the total number of time slices to be processed on lines 33 and 40
-# To change the metrics to use in determining optimal compression level, change the metrics (option -p) on line 74
+# To change the metrics to use in determining optimal compression level, change the metrics (option -p) on line 74 and 100
 conda activate my-npl-ml
-set prefix = testDaily
+set prefix = testAllDay
 
 #if ($0 == "new") then
 mkdir ../../data/${prefix}
 mkdir ../../data/${prefix}_calcs
 mkdir ../../data/${prefix}_calcs/reports
 
-set arrDay= (TS)
+set arrDay= (bc_a1_SRF dst_a1_SRF dst_a3_SRF FLNS FLNSC FLUT FSNS FSNSC FSNTOA ICEFRAC LHFLX pom_a1_SRF PRECL PRECSC PRECSL PRECT PRECTMX PSL Q200 Q500 Q850 QBOT SHFLX so4_a1_SRF so4_a2_SRF so4_a3_SRF soa_a1_SRF soa_a2_SRF T010 T200 T500 T850 TAUX TAUY TMQ TREFHT TREFHTMN TREFHTMX TS U010 U200 U500 U850 VBOT WSPDSRFAV Z050 Z500)
 set arrMonth= (TS)
 set arrComp = (zfp_p_10 zfp_p_12 zfp_p_14 zfp_p_16 zfp_p_18 zfp_p_20 zfp_p_22 zfp_p_24 zfp_p_26)
 set time = (0 1095 2190 3285 4380 5475 6570 7665 8760 9855 10950 12045 13140 14235 15330 16425 17520 18615 19710 20805 21900 22995 24090 25185 26280)
@@ -95,3 +95,10 @@ python create_dataframe.py -l ../../data/${prefix}_calcs/${prefix}_daily_labels.
 echo "dataframe created, running models"
 
 python ../data_analysis/models.py -d ../../data/${prefix}_calcs/${prefix}_daily_df.csv -e rf nn -t 1 -r ../../data/${prefix}_calcs/reports/
+
+#add a step here for formatting the tables and reports before converting to latex and saving
+
+echo "models completed, formatting report"
+foreach p (rf nn)
+  python tably.py ../../data/${prefix}_calcs/reports/${p}_report.csv -o ../../data/${prefix}_calcs/reports/${p}_report.tex
+end
