0e2ebdd6bd8eea01d70337473c878bb2446eac86
Alex <pinard.alex@gmail.com>
2022-10-11 09:19:06 -0600

    aggressive

--- lcr/data_gathering/data_gather_monthly.sh
+++ lcr/data_gathering/data_gather_monthly.sh
@@ -25,7 +25,7 @@
 conda activate my-npl-ml
 
 # directory and filename prefix
-set prefix = CAMmonthly
+set prefix = CAMmonthly95
 # "new" or "rerun" or "compress"
 set runtype = "rerun"
 # "fixed" or "random"
