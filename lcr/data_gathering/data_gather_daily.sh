ad1c4ed714884064a79912b712e841bd6a62b126
Alex <pinard.alex@gmail.com>
2022-12-19 12:01:04 -0800

    update

--- lcr/data_gathering/data_gather_daily.sh
+++ lcr/data_gathering/data_gather_daily.sh
@@ -25,15 +25,15 @@
 conda activate my-npl-ml
 
 # directory and filename prefix
-set prefix = bigDCQDay
+set prefix = bigICEFRACDay
 # "new" or "rerun" or "compress"
-set runtype = "calcs"
+set runtype = "new"
 # "fixed" or "random"
 set testset = "random"
 set alg = "zfp"
 
 #set arrDay= (bc_a1_SRF dst_a1_SRF dst_a3_SRF FLNS FLNSC FLUT FSNS FSNSC FSNTOA ICEFRAC LHFLX pom_a1_SRF PRECL PRECSC PRECSL PRECT PRECTMX PSL Q200 Q500 Q850 QBOT SHFLX so4_a1_SRF so4_a2_SRF so4_a3_SRF soa_a1_SRF soa_a2_SRF T010 T200 T500 T850 TAUX TAUY TMQ TREFHT TREFHTMN TREFHTMX TS U010 U200 U500 U850 UBOT V200 V500 V850 VBOT WSPDSRFAV Z050 Z500)
-set arrDay= (DCQ)
+set arrDay= (ICEFRAC)
 set arrMonth = ()
 
 #git pull
--- lcr/data_gathering/grab_all_TS.sh
+++ lcr/data_gathering/grab_all_TS.sh
@@ -5,7 +5,7 @@ import os
 # Full dataset
 #indir = "/glade/p/cisl/asap/abaker/pepsi/ens_31/orig/daily"
 # 2006-2010 subset daily
-indir = "/glade/p/cisl/asap/abaker/pepsi/ens_31/orig/daily/"
+indir = "/glade/p/cisl/asap/abaker/pepsi/ens_31/orig/daily"
 # 2006-2010 subset monthly
 #indir = "/glade/p/cisl/asap/CAM_lossy_test_data_31/research/monthly_orig/"
 
