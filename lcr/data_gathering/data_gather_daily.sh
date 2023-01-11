ddbc820f1f6ae4c7dca19dd34804c3af2c9f9b50
Alex <pinard.alex@gmail.com>
2023-01-11 11:11:17 -0700

    almost - still using "bg" though

--- lcr/data_gathering/data_gather_daily.sh
+++ lcr/data_gathering/data_gather_daily.sh
@@ -31,7 +31,7 @@ set prefix = AllCAMdaily4
 set runtype = "new"
 # "fixed" or "random"
 set testset = "calcs"
-set alg = "br"
+set alg = "bg"
 
 set arrDay= (bc_a1_SRF dst_a1_SRF dst_a3_SRF FLNS FLNSC FLUT FSNS FSNSC FSNTOA ICEFRAC LHFLX pom_a1_SRF PRECL PRECSC PRECSL PRECT PRECTMX PSL Q200 Q500 Q850 QBOT SHFLX so4_a1_SRF so4_a2_SRF so4_a3_SRF soa_a1_SRF soa_a2_SRF T010 T200 T500 T850 TAUX TAUY TMQ TREFHT TREFHTMN TREFHTMX TS U010 U200 U500 U850 UBOT V200 V500 V850 VBOT WSPDSRFAV Z050 Z500)
 #set arrDay= (ICEFRAC)
