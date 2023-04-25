352ca683499f97d89824999cc699aec87471fe75
Alex <pinard.alex@gmail.com>
2023-04-25 00:59:19 -0600

    new

--- lcr/data_analysis/CNN11.py
+++ lcr/data_analysis/CNN11.py
@@ -26,9 +26,9 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
 from tensorflow.keras.layers import Conv2D, MaxPooling2D
 from tensorflow.keras import models
-mpl.use('TKAgg')
+# mpl.use('TKAgg')
 
-#os.environ["HDF5_PLUGIN_PATH"]
+os.environ["HDF5_PLUGIN_PATH"]
 
 def actualsize(input_obj):
     memory_size = 0
