dc84c0fc0d8b8ebb470210b587f3db840301c01d
Alex <pinard.alex@gmail.com>
2023-09-12 10:41:19 -0600

    d

--- lcr/data_analysis/RFnew/echo.sh
+++ lcr/data_analysis/RFnew/echo.sh
@@ -43,7 +43,7 @@ foreach model ($models)
     conda activate my-npl-ml
     foreach feature ($features)
       # save all ids for line 48
-      set newid = `printf "tcsh -c 'module load conda && conda activate my-npl-ml && python main.py -j RF_TEMPLATE.json -m "${model}" -f "${feature}"'" | qsub -A NTDD0005 -N feature -q regular -l walltime=12:00:00 -j oe -M apinard@ucar.edu -l select=1:ncpus=1 -q casper`
+      set newid = `printf "tcsh -c 'setenv HDF5_PLUGIN_PATH /glade/work/haiyingx/H5Z-ZFP-PLUGIN-unbiased/plugin && module load conda && conda activate my-npl-ml && python main.py -j RF_TEMPLATE.json -m "${model}" -f "${feature}"'" | qsub -A NTDD0005 -N feature -q regular -l walltime=12:00:00 -j oe -M apinard@ucar.edu -l select=1:ncpus=1 -q casper`
       # remove the period and everything after it
       set newid = `echo $newid | sed 's/\..*//'`
       set ids = ( $ids $newid )
