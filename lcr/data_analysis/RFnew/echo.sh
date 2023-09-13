03ed44d57145607e64fa53adb3885f88c16c5353
Alex <pinard.alex@gmail.com>
2023-09-13 15:49:48 -0600

    df

--- lcr/data_analysis/RFnew/echo.sh
+++ lcr/data_analysis/RFnew/echo.sh
@@ -60,8 +60,8 @@ foreach model ($models)
     set str = 'setenv HDF5_PLUGIN_PATH /glade/work/haiyingx/H5Z-ZFP-PLUGIN-unbiased/plugin && module load conda && conda activate my-npl-ml && python main.py -j RF_TEMPLATE.json -m "${model}" --testset TESTSET -l ${features}'
     set str = `echo $str | sed "s/MODEL/${model}/"`
     set str = `echo $str | sed "s/FEATURES/${features}/"`
-    printf '%s\n' ${str}
+    printf '%s' ${str}
 
-    tcsh -c 'setenv HDF5_PLUGIN_PATH /glade/work/haiyingx/H5Z-ZFP-PLUGIN-unbiased/plugin && module load conda && conda activate my-npl-ml && python main.py -j RF_TEMPLATE.json -m MODEL --testset TESTSET -l FEATURES' | qsub -A NTDD0005 -N final -q casper -l walltime=12:00:00 -j oe -M apinard@ucar.edu -l select=1:ngpus=1:mem=40GB -l gpu_type=v100
+    tcsh -c str | qsub -A NTDD0005 -N final -q casper -l walltime=12:00:00 -j oe -M apinard@ucar.edu -l select=1:ngpus=1:mem=40GB -l gpu_type=v100
   endif
 end
