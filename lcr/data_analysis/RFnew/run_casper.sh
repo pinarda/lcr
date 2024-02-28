f0e744e06f3282b90f0c8b896f28db6754288c22
Alex <pinard.alex@gmail.com>
2024-02-28 11:05:03 -0700

    d

--- lcr/data_analysis/RFnew/run_casper.sh
+++ lcr/data_analysis/RFnew/run_casper.sh
@@ -11,7 +11,7 @@ python main2.py --onlydata False --runonlydata False --metric dssim -j RF_templa
 #rm -f /Users/alex/git/lcr/lcr/data_analysis/RFnew/*.h5
 python main2.py --onlydata True --runonlydata True --metric spre -j RF_template_spre.json --labelsonly True
 python main2.py --onlydata False --runonlydata False --metric spre -j RF_template_spre.json --labelsonly True
-#
+
 #rm -f /Users/alex/git/lcr/lcr/data_analysis/RFnew/data/*.npy
 #rm -f /Users/alex/git/lcr/lcr/data_analysis/RFnew/plots/*.npy
 #rm -f /Users/alex/git/lcr/lcr/data_analysis/RFnew/*.h5
