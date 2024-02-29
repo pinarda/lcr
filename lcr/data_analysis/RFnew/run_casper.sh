b8c45d6244a4bc640d06c8762c85d7c238f9dee2
Alex <pinard.alex@gmail.com>
2024-02-28 19:48:15 -0700

    test

--- lcr/data_analysis/RFnew/run_casper.sh
+++ lcr/data_analysis/RFnew/run_casper.sh
@@ -24,7 +24,7 @@ rm -f /Users/alex/git/lcr/lcr/data_analysis/RFnew/*.h5
 
 
 
-python main2.py --onlydata True --runonlydata True --metric dssim -j RF_template_dssim.json --labelsonly True --model cnn
+python main2.py --onlydata True --runonlydata True --metric dssim -j RF_template_dssim.json --labelsonly True --model rf
 #python main2.py --onlydata False --runonlydata False --metric dssim -j RF_template_dssim.json --labelsonly True
 #
 ##
