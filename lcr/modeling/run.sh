3915af4ad318e69ac9b56cac2c7d2265102afcd7
Alex <pinard.alex@gmail.com>
2024-11-04 20:51:50 -0700

    d

--- lcr/modeling/run.sh
+++ lcr/modeling/run.sh
@@ -19,7 +19,7 @@ for config_file in "$config_dir"/rotated_config_*.json; do
 ### Request a resource chunk with a GPU
 #PBS -l select=1:ngpus=1
 ### Specify that the GPUs will be V100s
-#PBS -l gpu_type=v100
+#PBS -l gpu_type=a100
 ### Allow job to run up to 12 hours
 #PBS -l walltime=12:00:00
 ### Route the job to the casper queue
