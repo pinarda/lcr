43c8013991b39ea4019b34b2193f1ae26ea17af6
Alex <pinard.alex@gmail.com>
2024-12-04 09:24:12 -0700

    d

--- lcr/modeling/mainplots1.py
+++ lcr/modeling/mainplots1.py
@@ -152,7 +152,7 @@ def main():
     for i in range(1, args.num_configs + 1):
         config_file = f"{args.config_prefix}{i}{args.suffix}"
         if os.path.exists(config_file):
-            if config_file != "rotated_config_23.json":
+            if i not in [23, 24]:
                 print(f"Processing {config_file}...")
                 (variable, weighted_cnn, macro_cnn,
                  weighted_rf, macro_rf, feature_importances, rf_feature_list) = process_config(config_file)
