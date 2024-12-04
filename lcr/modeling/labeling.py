d4a77c19d21a21b3e184d40316e331101fb07e74
Alex <pinard.alex@gmail.com>
2024-12-04 00:11:37 -0700

    n

--- lcr/modeling/labeling.py
+++ lcr/modeling/labeling.py
@@ -19,9 +19,18 @@ def labeling():
         "magnitude_range"
     ]
 
+    variables_order = [
+        "TREFHTMX", "TS", "LHFLX", "PRECSL", "PRECT", "PSL", "Q200", "Q500", "Q850",
+        "SHFLX", "T200", "T500", "T850", "TAUX", "TAUY", "U010", "FLNS", "FLNSC",
+        "FSNS", "FSNSC", "PRECL", "PRECSC", "QBOT", "T010", "TMQ", "TREFHT",
+        "TREFHTMN", "U200", "U500", "U850", "UBOT", "V200", "V500", "V850", "VBOT",
+        "WSPDSRFAV", "Z050", "Z500", "bc_a1_SRF", "dst_a1_SRF", "dst_a3_SRF",
+        "pom_a1_SRF", "so4_a1_SRF", "so4_a2_SRF", "so4_a3_SRF", "soa_a1_SRF", "soa_a2_SRF"
+    ]
+
     # Define the variable names in the desired order
-    variables_order = ['FLNS', 'LHFLX', 'PRECSL', 'PRECT', 'PSL', 'Q200', 'Q500', 'Q850',
-                       'SHFLX', 'T200', 'T500', 'T850', 'TAUX', 'TAUY', 'TREFHTMX', 'TS', 'U010']
+    # variables_order = ['FLNS', 'LHFLX', 'PRECSL', 'PRECT', 'PSL', 'Q200', 'Q500', 'Q850',
+    #                    'SHFLX', 'T200', 'T500', 'T850', 'TAUX', 'TAUY', 'TREFHTMX', 'TS', 'U010']
 
     # Assume there are 100 timesteps for each variable
     timesteps_per_variable = 100
@@ -32,7 +41,8 @@ def labeling():
     # Loop over each feature and process its corresponding file
     for feature in features:
         # Construct the filename based on the feature
-        file_path = f"{data_dir}FLNS_LHFLX_PRECSL_PRECT_PSL_Q200_Q500_Q850_SHFLX_T200_T500_T850_TAUX_TAUY_TREFHTMX_TS_U010_combined_lens1_ens25_1920_orig_FEATURE_{feature}_all_time100_second.nc"
+        file_path = f"{data_dir}TREFHTMX_TS_LHFLX_PRECSL_PRECT_PSL_Q200_Q500_Q850_SHFLX_T200_T500_T850_TAUX_TAUY_U010_FLNS_FLNSC_FSNS_FSNSC_PRECL_PRECSC_QBOT_T010_TMQ_TREFHT_TREFHTMN_U200_U500_U850_UBOT_V200_V500_V850_VBOT_WSPDSRFAV_Z050_Z500_bc_a1_SRF_dst_a1_SRF_dst_a3_SRF_pom_a1_SRF_so4_a1_SRF_so4_a2_SRF_so4_a3_SRF_soa_a1_SRF_soa_a2_SRF_combined_lens1_ens25_1920_orig_FEATURE_{feature}_all_time1600_second.nc"
+
 
         try:
             # Load the combined DataArray for this feature
@@ -42,7 +52,8 @@ def labeling():
             combined_data = combined_data.assign_coords(sample=("sample", new_sample_coord))
 
             # Save the modified data with labeled samples for this feature
-            output_filename = f"{data_dir}FLNS_LHFLX_PRECSL_PRECT_PSL_Q200_Q500_Q850_SHFLX_T200_T500_T850_TAUX_TAUY_TREFHTMX_TS_U010_labeled_combined_lens1_ens25_1920_orig_FEATURE_{feature}_all_time100_second.nc"
+            output_filename = f"{data_dir}TREFHTMX_TS_LHFLX_PRECSL_PRECT_PSL_Q200_Q500_Q850_SHFLX_T200_T500_T850_TAUX_TAUY_U010_FLNS_FLNSC_FSNS_FSNSC_PRECL_PRECSC_QBOT_T010_TMQ_TREFHT_TREFHTMN_U200_U500_U850_UBOT_V200_V500_V850_VBOT_WSPDSRFAV_Z050_Z500_bc_a1_SRF_dst_a1_SRF_dst_a3_SRF_pom_a1_SRF_so4_a1_SRF_so4_a2_SRF_so4_a3_SRF_soa_a1_SRF_soa_a2_SRF_combined_lens1_ens25_1920_orig_FEATURE_{feature}_all_time1600_second.nc"
+
             combined_data.to_netcdf(output_filename)
 
             print(f"Data for feature '{feature}' has been labeled and saved as '{output_filename}'")
