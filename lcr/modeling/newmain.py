bf393cb82bc5e0e4264edf52e95eda9e4ff232a9
Alex <pinard.alex@gmail.com>
2024-12-04 02:40:39 -0700

    d

--- lcr/modeling/newmain.py
+++ lcr/modeling/newmain.py
@@ -774,7 +774,8 @@ def main():
     # Plotting
     fig, ax = plt.subplots()
 
-    all_labels = list(confusion_df.index)
+    label_encoder = np.load(f"{storageloc}/label_encoder_{j}{time}{modeltype}{jobid}_{var_list[0]}.npy", allow_pickle=True)
+    all_labels = list(label_encoder.classes_)
 
     # Plot correct predictions in bold color
     ax.bar(all_labels, diagonal, label="Correct Predictions", color="blue", edgecolor="black")
