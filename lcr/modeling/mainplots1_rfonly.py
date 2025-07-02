4d87a342d8206d05cf435b7e65c6bad6fb3a6bc3
Alex <pinard.alex@gmail.com>
2025-07-02 17:01:55 -0600

    test

--- lcr/modeling/mainplots1_rfonly.py
+++ lcr/modeling/mainplots1_rfonly.py
@@ -42,7 +42,14 @@ def process_config(config_file):
     # test_predictions_cnn = np.load(f"{storageloc}/test_predictions_{j}{time}cnn{jobid}_{variable_file}.npy")
 
     # Load data for RF
-    test_labels_np_rf = np.load(f"{storageloc}/test_labels_{j}{time}rf{jobid}_{variable_file}.npy")
+    # test_labels_np_rf = np.load(f"{storageloc}/test_labels_{j}{time}rf{jobid}_{variable_file}.npy")
+    test_labels_np_rf = load_first_existing(
+        [
+            f"{storageloc}/test_labels_02000rf0_{variable_file}.npy",
+            f"{storageloc}/test_labels_12000rf0_{variable_file}.npy",
+        ],
+        allow_pickle=True
+    )
     # test_predictions_rf = np.load(f"{storageloc}/test_predictions_rf_{j}{time}rf{jobid}_{variable_file[0]}.npy")
     test_predictions_rf = load_first_existing(
         [
