2b81d97f574afa1589d395a8d6f66d6ef158ae93
Alex <pinard.alex@gmail.com>
2025-07-17 13:58:39 -0600

    test

--- lcr/modeling/mainplots1_rfonly.py
+++ lcr/modeling/mainplots1_rfonly.py
@@ -203,7 +203,7 @@ def plot_feature_importances(features, all_importances, variable_names, filename
             x + i * bar_width,  # Shift each group by `i * bar_width`
             importances,
             bar_width,
-            label=variable_names[i][0]
+            label=variable_names[i]
         )
 
     # Add labels and legend
