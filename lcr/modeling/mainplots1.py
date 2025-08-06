76b9c3d9133b7f94ae12effaa6760cd658a405e2
Alex <pinard.alex@gmail.com>
2025-08-06 16:22:56 -0600

    test

--- lcr/modeling/mainplots1.py
+++ lcr/modeling/mainplots1.py
@@ -201,9 +201,9 @@ def main():
     for i in range(1, args.num_configs + 1):
         config_file = f"{args.config_prefix}{i}{args.suffix}"
         if os.path.exists(config_file):
-            if i not in [20]:
+            # if i not in [23, 24, 36, 37, 40, 41, 43, 44]:
             # if i not in [1, 2, 4, 5, 12, 13, 16, 19, 20, 21, 22, 24, 26, 27, 37, 43, 44, 45, 46, 47]:
-            if i not in [11, 16, 18, 24, 27]:
+            if i not in [20]:
                 print(f"Processing {config_file}...")
                 (variable, weighted_cnn, macro_cnn,
                  weighted_rf, macro_rf, feature_importances, rf_feature_list, df_counts) = process_config(config_file)
