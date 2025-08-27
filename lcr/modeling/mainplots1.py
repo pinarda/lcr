7c179270f45967aa4f2f0879eadcac550e99c1c6
Alex <pinard.alex@gmail.com>
2025-08-26 22:45:02 -0600

    test

--- lcr/modeling/mainplots1.py
+++ lcr/modeling/mainplots1.py
@@ -50,6 +50,7 @@ def process_config(config_file):
             f"{storageloc}/test_labels_12000rf0_{variable_file}.npy",
             f"{storageloc}/test_labels_22000rf0_{variable_file}.npy",
             f"{storageloc}/test_labels_32000rf0_{variable_file}.npy",
+            f"{storageloc}/test_labels_42000rf0_{variable_file}.npy",
         ],
         allow_pickle=True
     )
@@ -62,6 +63,7 @@ def process_config(config_file):
             f"{storageloc}/test_predictions_rf_12000rf0_{variable_file}.npy",
             f"{storageloc}/test_predictions_rf_22000rf0_{variable_file}.npy",
             f"{storageloc}/test_predictions_rf_32000rf0_{variable_file}.npy",
+            f"{storageloc}/test_predictions_rf_42000rf0_{variable_file}.npy",
         ],
         allow_pickle=True
     )
