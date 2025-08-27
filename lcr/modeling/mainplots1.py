216b4676fce409ff85dea982495815d1612e56c0
Alex <pinard.alex@gmail.com>
2025-08-26 23:04:26 -0600

    test

--- lcr/modeling/mainplots1.py
+++ lcr/modeling/mainplots1.py
@@ -246,7 +246,7 @@ def main():
         if os.path.exists(config_file):
             # if i not in [23, 24, 36, 37, 40, 41, 43, 44]:
             # if i not in [1, 2, 4, 5, 12, 13, 16, 19, 20, 21, 22, 24, 26, 27, 37, 43, 44, 45, 46, 47]:
-            if i not in [20, 1, 24]:
+            if i not in [20, 1, 24, 25]:
                 print(f"Processing {config_file}...")
                 (variable, weighted_cnn, macro_cnn,
                  weighted_rf, macro_rf, feature_importances, rf_feature_list, df_counts) = process_config(config_file)
