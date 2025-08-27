8ca9e666b7ba683ab31367d0662f7cb30601e977
Alex <pinard.alex@gmail.com>
2025-08-26 23:33:25 -0600

    test

--- lcr/modeling/run1.sh
+++ lcr/modeling/run1.sh
@@ -4,7 +4,7 @@
 config_dir="." # Change this if your config files are in a different directory
 
 # Specify the single config file to process
-config_file="$config_dir/rotated_config_48.json"
+config_file="$config_dir/rotated_config_71.json"
 
 # Check if the specified config file exists
 if [ -f "$config_file" ]; then
